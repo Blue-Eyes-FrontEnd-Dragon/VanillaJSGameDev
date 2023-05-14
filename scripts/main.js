cf5ff29fc716c0fa960f98c70ea4376aa9ff46f8
LukeMcCann <developer-lukemccann@outlook.com>
2023-05-14 15:22:05 +0100

    Animate as running dog

--- scripts/main.js
+++ scripts/main.js
@@ -11,18 +11,36 @@ const PLAYER_SPRITESHEET = {
 	heightInPx: 5230,
 }
 
+const FRAME_DELAY = 200;
+
 const playerImage = new Image();
 playerImage.src = '../images/shadow_dog.png';
 
 const spriteWidth = (PLAYER_SPRITESHEET.widthInPx / PLAYER_SPRITESHEET.columns) + 2;
 const spriteHeight = (PLAYER_SPRITESHEET.heightInPx / PLAYER_SPRITESHEET.rows);
 
+let x = 1;
+let y = 1;
+
 function animate() {
 	ctx.clearRect(0, 0, CANVAS_WIDTH,CANVAS_HEIGHT);
 
-	ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+	ctx.drawImage(playerImage, spriteWidth * x, spriteHeight * y, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
-	requestAnimationFrame(animate);
+	x++;
+	y++;
+
+	if (x > PLAYER_SPRITESHEET.rows - 8) {
+		x = 0;
+	}
+
+	if (y > PLAYER_SPRITESHEET.columns - 10) {
+		y = 0;
+	}
+
+	setTimeout(() => {
+		requestAnimationFrame(animate);
+	}, FRAME_DELAY);
 }
 
 animate();
